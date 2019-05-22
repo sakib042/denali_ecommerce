ef766add0f698ba3b9b69f67d3d1fbbc1990d636
Nazmus Sakib <sakib042@gmail.com>
2019-05-23 04:24:03 +0600

    done

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,4 +8,59 @@ $(document).ready(function () {
         $(".navbar-menu").toggleClass("is-active");
 
     });
+
+    $('.client-slider').slick({
+        autoplay: true,
+        infinite: true,
+        mobileFirst: true,
+        pauseOnHover: true,
+        autoplaySpeed: 3000,
+        dots: false,
+        arrow: false,
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        responsive: [{
+                breakpoint: 1400,
+                settings: {
+                    slidesToShow: 5
+                }
+            },
+            {
+                breakpoint: 1023,
+                settings: {
+                    slidesToShow: 4
+                }
+            },
+            {
+                breakpoint: 557,
+                settings: {
+                    slidesToShow: 3
+                }
+            }
+        ]
+    });
+
+    // Smooth scroll
+
+    // Add smooth scrolling to all links
+    $("a.smooth").on('click', function (event) {
+
+        // Make sure this.hash has a value before overriding default behavior
+        if (this.hash !== "") {
+            // Prevent default anchor click behavior
+            event.preventDefault();
+
+            // Store hash
+            var hash = this.hash;
+
+            // Using jQuery's animate() method to add smooth page scroll
+            // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function () {
+                // Add hash (#) to URL when done scrolling (default click behavior)
+                window.location.hash = hash;
+            });
+        } // End if
+    });
 });
